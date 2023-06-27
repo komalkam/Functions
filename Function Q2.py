9689121c551a9be0e478870831d009e2507542b5
komalkam <136676732+komalkam@users.noreply.github.com>
2023-06-27 12:13:54 +0530

    Add files via upload

---
+++ Function Q2.py
@@ -0,0 +1,6 @@
+def reverse_string(input_string):
+    reversed_string = input_string[::-1]
+    return reversed_string
+sample_string = "1234abcd"
+result = reverse_string(sample_string)
+print("Reversed String:", result)
