12beb98c22bcc254b67ed8c91612fa852848d07f
komalkam <136676732+komalkam@users.noreply.github.com>
2023-07-08 10:30:01 +0530

    Add files via upload

---
+++ Assignment4_Q2.py
@@ -0,0 +1,11 @@
+def triple(num):
+    return num * 3
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+result = list(map(triple, numbers))
+
+print("Triple of list numbers:")
+print(result)
+
+print("Triple of list numbers:")
+print(result)
